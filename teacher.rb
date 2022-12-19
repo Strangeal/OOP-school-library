8fabb047927cc31e86eb7276b20e747251c683ba
strangeal <bajerijustice@gmail.com>
2022-12-19 02:51:16 -0800

    Add person.rb, student.rb and teacher.rb files

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require_relative 'person'
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization)
+    super
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
